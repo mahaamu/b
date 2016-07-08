115e56c3159bce325b07f6026f3a0d1b0951b869
mahaamu <mahalakshmi3901@gmail.com>
2016-07-08 14:35:06 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,18 @@
+# b
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+int main()
+{
+char check;
+scanf("%c",&check);
+if(isdigit(check))
+{
+printf("\nnumber");
+}
+  else
+	{	
+  	printf("\ncharacter");
+	}
+return 0;
+}
